9629bd789219f8de21add75a0bcb2022a6158533
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-08-02 10:07:30 -0300

    added webpack webserver

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
         filename: 'direct.js',
         path: __dirname + '/build'
     },
+    devServer: {
+        contentBase: "./build",
+        port: 9000
+    },
     optimization: {
         minimizer: [
             new UglifyJsPlugin({
